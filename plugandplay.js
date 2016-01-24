38e482ff6a584cbd09a9decfff97cf7ba8bf0a58
Tejas Katariya <tkatariya@internal.tavisca.com>
2016-01-24 16:39:49 +0530

    Flights - completed html part.

--- plugandplay.js
+++ plugandplay.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	
-	var flightImgUrl = 'http://gallsource.com/wp-content/uploads/2014/08/Aircraft-Height-Sky-Wings-Clouds-05E-Down-Beautiful-HD-Wallpapers.jpg';
+	var flightImgUrl = 'http://wallpapershacker.com/wallpaper/airplanes_liftoff_scandinavian_airlines_hd-wallpaper-1036118.jpg';
 	var hotelsImgUrl = 'http://www.thewallpapers.org/photo/30446/luxury-hotel-concept.jpg';
 	var carsImgUrl = 'http://www.shortday.in/wp-content/uploads/2015/05/Car-hd-wallpapers.jpg';
 	var activityImgUrl = 'http://wallpapercave.com/wp/dcQv50O.jpg';
@@ -26,10 +26,21 @@ $(document).ready(function(){
 	$('.packagesTab').click(function(){
 		$('body').css('background-image',"url("+packagesImgUrl+")" );
 	});
-	
-	
-	
-	
-	
+    
+    $('input[data-toggle="tooltip"]').tooltip({
+				delay: 500,
+				trigger: 'hover',
+				placement:'top'				
+    });
+				
+    $('.datepicker').datetimepicker({
+        format: 'MM/DD/YYYY',
+        minDate: moment(1, 'h')
+    });
+			
+			$('.timepicker').datetimepicker({
+				format: 'LT'
+			});
+			
 	
 });
